9ad3a86fefbf523b13a37d9747e458383e2383cc
1HenriqueMata1 <57409783+1HenriqueMata1@users.noreply.github.com>
2021-03-18 10:29:08 +0000

    Create ticTacToe copy.py

---
+++ ticTacToe copy.py
@@ -0,0 +1,42 @@
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-----')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-----')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+def switch(arg):
+    switcher= {
+        1: 'top-L',
+        2: 'top-M',
+        3: 'top-R',
+        4: 'mid-L',
+        5: 'mid-M',
+        6: 'mid-R',
+        7: 'low-L',
+        8: 'low-M',
+        9: 'low-R',
+    }
+    return switcher.get(arg, "Invalid number!")
+
+turn = 'X'
+for i in range(9):
+    printBoard(theBoard)
+    print('Turn for ' + turn + '. Move on which space?')
+    move = int(input())
+    while (move < 1) or (move > 9):
+        print('Try again!')
+        break
+
+    theBoard[switch(move)] = turn
+
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
+
+printBoard(theBoard)      
