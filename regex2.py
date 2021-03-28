514865dabb9fc31126c4d28ad6597a5811d8fd55
1HenriqueMata1 <57409783+1HenriqueMata1@users.noreply.github.com>
2021-03-28 22:04:47 +0100

    comments

--- regex2.py
+++ regex2.py
@@ -1,5 +1,6 @@
 import re
 
+#IDLE
 robocop = re.compile(r'robocop', re.I)
 robocop.search('RoboCop is bullshit!')
 robocop.search('RoboCop is bullshit!').group()
@@ -9,4 +10,7 @@ namesRegex.sub('CENSORED', 'Agent Alice gave the secret to Agent Bob, who is a b
 
 agentNameRegex = re.compile(r'Agent (\w)\w*')
 agentNameRegex.sub(r'\1****', 'Agent Alice who is dumb and Agent Bob fucked Agent Helen')
+
+#página 207 apagar
+# ? DAMN
 
