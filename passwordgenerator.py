45abea9b686ecc43bb9399f5a2966a0555160442
1HenriqueMata1 <57409783+1HenriqueMata1@users.noreply.github.com>
2021-04-27 17:30:55 +0100

    Update passwordgenerator.py

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,11 +1,29 @@
 import random
 import string
+import win32api
 
 LETTERS = string.ascii_letters
 NUMS = '0123456789'
 SPE = '-_.:,;+*@£$%?!€'
 SYMBOLS = LETTERS + NUMS + SPE
-len = 25
-for i in range(1,5):
-    password = ''.join(random.sample(SYMBOLS , len))
-    print ("Password option " + str(i) + " : " + password) 
+palavrapasse = '12'
+len = 2
+
+def code():
+    attempt = ''.join(random.sample(SYMBOLS, len))
+    count = 0
+    while (attempt != palavrapasse):
+        attempt = ''.join(random.sample(SYMBOLS, len))
+        print(attempt)
+        count = count + 1
+        if (attempt == palavrapasse):
+            print("Password was: " + palavrapasse)
+            print("Number of attempts: " + str(count))
+            win32api.MessageBox(0, 'Password found', 'Message', 0x00001000)
+
+code()
+
+def generatorpass():
+    for i in range(1, 5):
+        password = ''.join(random.sample(SYMBOLS, len))
+        print("Password option " + str(i) + " : " + password)
