843a8adb641268c8534e39b1b50605f040499a7f
H3NR1QU3M4LT4 <57409783+H3NR1QU3M4LT4@users.noreply.github.com>
2021-06-04 15:31:21 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,11 +3,9 @@ import random, sys, os, math
 
 client = discord.Client()
 num = random.randint(0, 9)
-keywords=["fds", "teste", "diagramas", "uia", "ahahah", "coisa", "sim", "não", "s", "n", "@MarcoFerreira", "ya", "missa", "louco", "gajo"]
+keywords=[""]
 
-listinsults=["Vai pró caralho pah", "És mesmo um zé do caralho", "E se te fosses foder? Não?", 
-"Vai-te foder masé pah!", "Que é queres? Deves ter um atraso tu.... Palhaço do caralho",
-"Olha-me este! FODASSE PAH"]
+listinsults=[""]
 
 @client.event
 async def on_message(message):
@@ -16,6 +14,6 @@ async def on_message(message):
             for j in range(1):
                 await message.channel.send("Vai pró caralho pah")
 
-client.run('ODIyMTg4NTgyODMxMjU5NzM5.YFOoyQ.PwXA4PzabObsZbIdv5PEntM25lQ')
+client.run('TOKEN')
 
 
